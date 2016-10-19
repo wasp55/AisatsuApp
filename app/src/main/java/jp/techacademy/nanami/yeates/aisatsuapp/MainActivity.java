16381604cc4de6e91a0fd9ae0e6dd1309434394f
Nanami <ku_kai73@yahoo.co.jp>
2016-10-19 20:40:40 +1000

    二回目

--- app/src/main/java/jp/techacademy/nanami/yeates/aisatsuapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/nanami/yeates/aisatsuapp/MainActivity.java
@@ -9,12 +9,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
- public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mGreeting;
     private Button mPickTime;
-
-    static final int TIME_DIALOG_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,34 +24,35 @@ import android.widget.TimePicker;
         mPickTime = (Button) findViewById(R.id.button1);
         mPickTime.setOnClickListener(this);
     }
-            @Override
-            public void onClick(View v){
-                Log.d("UI_PARTS", "ボタンをタップしました");
-                showTimePickerDialog();
 
+    @Override
+    public void onClick(View v) {
+        Log.d("UI_PARTS", "ボタンをタップしました");
+        showTimePickerDialog();
     }
 
-            private void showTimePickerDialog() {
-                TimePickerDialog timePickerDialog = new TimePickerDialog(this,
-                        new TimePickerDialog.OnTimeSetListener() {
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay, int minute){
+    private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 
-                                int time = hourOfDay;
-                                if ((time >=2) && (time <= 10)) {
-                                    mGreeting.setText("おはよう");
-                                }else if((time >=10) && (time <= 18)) {
-                                    mGreeting.setText("こんにちは");
-                                }else if((time >=18)|| (time <= 2)) {
-                                    mGreeting.setText("こんばんわ");
-                                }
+                        int time1 = hourOfDay;
+                        int time2 = minute;
+                        if ((time1 >= 2) && (time1 <= 9)) {
+                            mGreeting.setText("おはよう");
+                        } else if ((time1 >= 10) && (time1 <= 17)) {
+                            mGreeting.setText("こんにちは");
+                        } else if ((time1 >= 18) || (time1 <= 2)) {
+                            mGreeting.setText("こんばんわ");
+                        }
 
-                            }
-                        },
-                        0,// 初期値（時間）
-                        0,  // 初期値（分）
-                        true);
-                timePickerDialog.show();
-            }
-        }
+                    }
+                },
+                0,// 初期値（時間）
+                0,  // 初期値（分）
+                true);
+        timePickerDialog.show();
+    }
+}
 
